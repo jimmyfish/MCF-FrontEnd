ef9d56c91af4aa3899601a86f020f574c8d9d8ee
Jimmy Fish <killcoder212@gmail.com>
2016-10-05 16:45:03 +0700

    Adding New Assets

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,26 +1,26 @@
-$(function() {
+$(function () {
 
-    $(document).ready(function() {
-        $(".nav-tabs a").click(function() {
+    $(document).ready(function () {
+        $(".nav-tabs a").click(function () {
             $(this).tab('show');
         });
 
-        $('a.menu1').click(function() {
-          $('div.control div#firstController').removeClass('hide');
-          $('div.control div#secondController').addClass('hide');
+        $('a.menu1').click(function () {
+            $('div.control div#firstController').removeClass('hide');
+            $('div.control div#secondController').addClass('hide');
         });
-        $('a.menu2').click(function() {
-          $('div.control div#secondController').removeClass('hide');
-          $('div.control div#firstController').addClass('hide');
+        $('a.menu2').click(function () {
+            $('div.control div#secondController').removeClass('hide');
+            $('div.control div#firstController').addClass('hide');
         });
 
     });
-    
 
 
 
 
-    $('a[href*="#content-"]').click(function() {
+
+    $('a[href*="#content-"]').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -37,9 +37,11 @@ $(function() {
         interval: false
     });
 
+    $('a.navbar-brand-mini').css('margin-left', 'calc(0.25 * ' + window.innerWidth);
+
     // for every slide in carousel, copy the next slide's item in the slide.
     // Do the same for the next, next item.
-    $('.multi-item-carousel .item').each(function() {
+    $('.multi-item-carousel .item').each(function () {
         var next = $(this).next();
         var previous = $(this).prev();
         if (!next.length) {
@@ -56,20 +58,20 @@ $(function() {
 
     $('li.sign-up>.trigger').popover({
         html: true,
-        content: function() {
+        content: function () {
             return $(this).parent().find('.content').html();
         }
     });
     $('li.sign-in>.trigger').popover({
         html: true,
-        content: function() {
+        content: function () {
             return $(this).parent().find('.content').html();
         }
 
     });
 
-    $('body').on('click', function(e) {
-        $('[data-toggle="popover"]').each(function() {
+    $('body').on('click', function (e) {
+        $('[data-toggle="popover"]').each(function () {
             if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
                 $(this).popover('hide');
             }
